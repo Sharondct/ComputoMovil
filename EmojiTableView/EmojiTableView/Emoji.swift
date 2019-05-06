2da1d500b15eff03f697a3f6dd7b37efd1a76959
Sharon Reyes <sharon11.rc@gmail.com>
2019-05-06 12:53:57 -0500

    Add files via upload

---
+++ EmojiTableView/EmojiTableView/Emoji.swift
@@ -0,0 +1,23 @@
+//
+//  Emoji.swift
+//  EmojiTableView
+//
+//  Created by macbook on 5/6/19.
+//  Copyright © 2019 Sharon. All rights reserved.
+//
+
+class Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+    
+    init(symbol: String, name: String, description:
+    String,
+         usage: String) {
+        self.symbol = symbol
+        self.name = name
+        self.description = description
+        self.usage = usage
+    }
+}
