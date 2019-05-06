6558d3f010fbc7f3f6592e611231dd8c2d47c984
Sharon Reyes <sharon11.rc@gmail.com>
2019-05-06 12:59:18 -0500

    Add files via upload

---
+++ RainbowApp/RainbowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RainbowApp
+//
+//  Created by macbook on 4/22/19.
+//  Copyright © 2019 Sharon. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBarItem.badgeValue = "2"
+    }
+
+
+}
+
